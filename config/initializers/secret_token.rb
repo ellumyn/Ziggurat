bf41e16ff6e0086d933ed3972ac08c23928a8d60
Christian Agupitan <cd.agupitan@gmail.com>
2012-09-14 16:12:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ziggurat::Application.config.secret_token = '8edf01fba4ef75e36ce50fec46038985b9a0d75cb990e7d328c6d7260b9d30738cd71640acd11daceedd482a48ddfd01b1a6b5864c2f273e0f8f5ff8bf4c41f9'
